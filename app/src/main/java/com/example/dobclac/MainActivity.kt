237185da16575c3cfbe847fbd9c55a2682d1e986
Dawon00 <da6921@naver.com>
2022-10-30 20:39:53 +0900

    FEAT: DatePicker

--- app/src/main/java/com/example/dobclac/MainActivity.kt
+++ app/src/main/java/com/example/dobclac/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.dobclac
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener{ view ->
+
+
+            clickDatePicker()
+        }
+    }
+    fun clickDatePicker(){
+
+        val mayCalendar = Calendar.getInstance()
+        val year = mayCalendar.get(Calendar.YEAR)
+        val month = mayCalendar.get(Calendar.MONTH)
+        val day = mayCalendar.get(Calendar.DAY_OF_MONTH)
+
+
+        DatePickerDialog(this,
+            { view, year, month, dayOfMonth ->
+            Toast.makeText(this, "btnDatePicker works", Toast.LENGTH_LONG).show()
+        },
+            year,
+            month,
+            day
+             ).show()
+
     }
 }
